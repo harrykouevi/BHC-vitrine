eb7cd04b089807a44e36e014a29c37b380265601
Harry.kouevi <harry.kouevi@gmaol.com>
2024-12-30 19:35:16 +0000

    email on user table modified

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->string('email',500)->unique();
+            // $table->string('email',100)->unique();
+            $table->string('email',100);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
